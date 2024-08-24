bee99f6f932e9e601cd59da604160901c4519e43
Saulo Moreira <saulo.node@gmail.com>
2024-08-23 22:34:22 -0300

    ports nmap

--- cyberSocket.py
+++ cyberSocket.py
@@ -5,7 +5,7 @@ ports = [21, 23, 80, 443, 8080]
 for port in ports:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(0.1)
-    code = client.connect_ex(('nadinepsi.com.br', port))
+    code = client.connect_ex(('site.com', port))
     if code == 0:
         print(f'Port: {port} open')
     else:
